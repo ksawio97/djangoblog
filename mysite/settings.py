8e3023a915ce1f3ef9460afc51d4dc2392e388c9
Ksawery Kuczynski <ksawerykuczynskikk@gmail.com>
2024-06-24 20:54:05 +0200

    Fixed css not loading in production error

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIR = os.path.join(BASE_DIR, 'blog', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
